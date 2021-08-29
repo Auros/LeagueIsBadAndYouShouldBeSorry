e8f929cf0b8cb88694ddd7ddb7b0e412717ecd9d
Auros <aurosnex@gmail.com>
2021-08-29 19:38:45 -0400

    Add project files.

---
+++ LeagueIsBadAndYouShouldBeSorry/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using IPA;
+using Microsoft.Win32;
+using System.IO;
+using UnityEngine;
+
+namespace LeagueIsBadAndYouShouldBeSorry
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        [Init]
+        public Plugin()
+        {
+
+        }
+
+        [OnEnable]
+        public void OnEnable()
+        {
+            var key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\WOW6432Node\Riot Games, Inc\League of Legends");
+
+            if (key == null)
+                return;
+
+            if (Directory.Exists(key.GetValue("Location") as string))
+                Application.Quit();
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+
+        }
+    }
+}
